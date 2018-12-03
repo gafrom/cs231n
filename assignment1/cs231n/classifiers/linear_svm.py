fc409594f0828426418c76246575c47d886c0fa6
Afanasyev Oleg <gafrom@gmail.com>
2018-12-03 19:08:23 +0300

    Implement computation of gradient of the loss function by means of analytic calculus

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -28,16 +28,26 @@ def svm_loss_naive(W, X, y, reg):
   for i in range(num_train):
     scores = X[i].dot(W)
     correct_class_score = scores[y[i]]
+    
+    # dW calculation
+    margin_vector = scores - correct_class_score + 1
+    margin_vector[y[i]] = 0 # skipping correct class
+    num_positive_margins = np.count_nonzero(margin_vector > 0)
+
     for j in range(num_classes):
       if j == y[i]:
-        continue
+        dW[:, j] += -X[i] * num_positive_margins
+        continue # skip true class to only loop over incorrect classes
+
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
         loss += margin
+        dW[:, j] += X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
